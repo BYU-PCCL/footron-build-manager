db2dec9e7fe9f2085f2d74ff4f2ca414b8e851d2
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2022-08-03 16:59:41 -0600

    Change default config path

--- footron_build_manager/constants.py
+++ footron_build_manager/constants.py
@@ -8,7 +8,8 @@ GITHUB_WEBHOOK_SECRET = os.environ.get("GITHUB_WEBHOOK_SECRET")
 GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN")
 
 CONFIG_PATH = Path(
-    os.environ.get("FT_CONFIG_PATH") or XDG_CONFIG_HOME / "footron" / "config.toml"
+    os.environ.get("FT_CONFIG_PATH")
+    or XDG_CONFIG_HOME / "footron" / "build-config.toml"
 )
 DATA_PATH = Path(
     os.environ.get("FT_DATA_PATH") or XDG_DATA_HOME / "footron" / "build.json"
